cb7591c19e673af86f3fdf26bcf21b6fe3837b67
Liam Stanley <liam@lstanley.wks.liquidweb.com>
2016-07-17 17:44:40 -0400

    split things out, start generating structure

--- marill.go
+++ marill.go
@@ -1,138 +1,29 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"net/http"
 	"os"
-	"strings"
-
-	"golang.org/x/net/html"
+	"runtime"
+	"sync"
 )
 
-// Helper function to pull the src attribute from a Token
-func getSrc(t html.Token, attr string, req *http.Request) (string, error) {
-	var src string
-
-	for _, a := range t.Attr {
-		if a.Key == attr {
-			src = a.Val
-			break
-		}
-	}
-
-	if len(src) == 0 || strings.HasSuffix(src, "/") {
-		return "", errors.New("No src attribute found")
-	}
-
-	if strings.HasPrefix(src, "./") {
-		src = req.URL.Scheme + "://" + req.URL.Host + req.URL.Path + strings.SplitN(src, "./", 2)[1]
-	}
-
-	if strings.HasPrefix(src, "//") {
-		src = req.URL.Scheme + ":" + src
-	}
-
-	if strings.HasPrefix(src, "/") {
-		src = req.URL.Scheme + "://" + req.URL.Host + src
-	}
-
-	if !strings.HasPrefix(src, "http") {
-		return "", errors.New("Not a valid URL")
-	}
-
-	return src, nil
-}
-
-// Extract all http** links from a given webpage
-func crawl(url string, ch chan string, chFinished chan bool) {
-	resp, err := http.Get(url)
-
-	defer func() {
-		// Notify that we're done after this function
-		chFinished <- true
-	}()
-
-	if err != nil {
-		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
-		return
-	}
-
-	b := resp.Body
-	defer b.Close() // close Body when the function returns
-
-	z := html.NewTokenizer(b)
-
-	for {
-		tt := z.Next()
-
-		switch {
-		case tt == html.ErrorToken:
-			// End of the document, we're done
-			return
-		case tt == html.StartTagToken:
-			t := z.Token()
-
-			allowed := map[string]string{
-				"link":   "href",
-				"script": "src",
-				"img":    "src",
-			}
-			var isInAllowed bool
-			var checkType string
-			for key := range allowed {
-				if t.Data == key {
-					isInAllowed = true
-					checkType = allowed[key]
-					break
-				}
-			}
-
-			if !isInAllowed {
-				continue
-			}
-
-			// Extract the src value, if there is one
-			src, err := getSrc(t, checkType, resp.Request)
-			if err != nil {
-				fmt.Println(src, err)
-				continue
-			}
-
-			// Make sure the src begines in http**
-			ch <- src
-		}
-	}
-}
-
 func main() {
-	foundUrls := make(map[string]bool)
-	seedUrls := os.Args[1:]
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// Channels
-	chUrls := make(chan string)
-	chFinished := make(chan bool)
+	urlsToCheck := os.Args[1:]
+	results := []*Results{}
+	var wg sync.WaitGroup
 
-	// Kick off the crawl process (concurrently)
-	for _, url := range seedUrls {
-		go crawl(url, chUrls, chFinished)
+	for _, url := range urlsToCheck {
+		wg.Add(1)
+
+		go func(url string) {
+			defer wg.Done()
+
+			results = append(results, Crawl(url))
+		}(url)
 	}
 
-	// Subscribe to both channels
-	for c := 0; c < len(seedUrls); {
-		select {
-		case url := <-chUrls:
-			foundUrls[url] = true
-		case <-chFinished:
-			c++
-		}
-	}
+	wg.Wait()
 
-	fmt.Println("\nFound", len(foundUrls), "unique urls:")
-
-	for url := range foundUrls {
-		fmt.Println(" - " + url)
-	}
-
-	close(chUrls)
+	// print out cool stuff here based on results
 }
--- scraper.go
+++ scraper.go
@@ -1,1 +1,137 @@
 package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Results -- struct returned by Crawl() to represent the entire crawl process
+type Results struct {
+	Resource
+	Body      string
+	Resources []*Resource
+}
+
+// Resource represents a single entity of many within a given crawl. These should
+// only be of type css, js, jpg, png, etc (static resources).
+type Resource struct {
+	URL           string
+	Error         error
+	Code          int
+	Proto         string
+	ContentLength int64
+	TLS           *tls.ConnectionState
+}
+
+// getSrc crawls the body of the Results page, yielding all img/script/link resources
+// so they can later be fetched.
+func getSrc(b io.ReadCloser, req *http.Request) (urls []string) {
+	urls = []string{}
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			allowed := map[string]string{
+				"link":   "href",
+				"script": "src",
+				"img":    "src",
+			}
+			var isInAllowed bool
+			var checkType string
+			var src string
+
+			for key := range allowed {
+				if t.Data == key {
+					isInAllowed = true
+					checkType = allowed[key]
+					break
+				}
+			}
+
+			if !isInAllowed {
+				continue
+			}
+
+			for _, a := range t.Attr {
+				if a.Key == checkType {
+					src = a.Val
+					break
+				}
+			}
+
+			if len(src) == 0 || strings.HasSuffix(src, "/") {
+				continue
+			}
+
+			if len(req.URL.Path) == 0 {
+				req.URL.Path = "/"
+			}
+
+			if strings.HasPrefix(src, "./") {
+				src = req.URL.Scheme + "://" + req.URL.Host + req.URL.Path + strings.SplitN(src, "./", 2)[1]
+			}
+
+			if strings.HasPrefix(src, "//") {
+				src = req.URL.Scheme + ":" + src
+			}
+
+			if strings.HasPrefix(src, "/") {
+				src = req.URL.Scheme + "://" + req.URL.Host + src
+			}
+
+			if !strings.HasPrefix(src, "http") {
+				continue
+			}
+
+			urls = append(urls, src)
+		}
+	}
+}
+
+// Crawl manages the fetching of the main resource, as well as all child resources,
+// providing a Results struct containing the entire crawl data needed
+func Crawl(url string) (res *Results) {
+	res = &Results{}
+	resp, err := http.Get(url)
+
+	if err != nil {
+		res.Error = err
+		return
+	}
+
+	b := resp.Body
+	defer b.Close()
+
+	res.URL = url
+	res.Code = resp.StatusCode
+	res.Proto = resp.Proto
+	res.ContentLength = resp.ContentLength
+	res.TLS = resp.TLS
+
+	urls := getSrc(b, resp.Request)
+
+	bbytes, err := ioutil.ReadAll(b)
+	if len(bbytes) != 0 {
+		res.Body = string(bbytes[:])
+	}
+
+	fmt.Println(urls)
+
+	// here we should fetch all of the other resources in a different goroutine
+
+	return
+}
