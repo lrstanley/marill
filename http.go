4ab104f397125af7bea818a418c26ae094562b9c
Liam Stanley <me@liamstanley.io>
2016-07-21 15:28:48 -0400

    some extra documentation

--- http.go
+++ http.go
@@ -12,6 +12,10 @@ func Get(url string, ip string) (*http.Response, error) {
 		return nil, err
 	}
 
+	// if an IP address is provided, rewrite the Host headers
+	// of note: if we plan to support custom ports, these should be rewritten
+	// within the header. E.g. "hostname.com:8080" -- though, common ports like
+	// 80 and 443 are left out.
 	if len(ip) > 0 {
 		req.Host = ip
 	}
@@ -20,6 +24,7 @@ func Get(url string, ip string) (*http.Response, error) {
 	// setup to deny by a specific useragent string (or lack there of)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36")
 
+	// actually make the request here
 	resp, err := client.Do(req)
 
 	return resp, err
