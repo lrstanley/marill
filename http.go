f4db7a93a558ff9ba3bfaf656f979fdfd414e692
Liam Stanley <me@liamstanley.io>
2016-07-21 16:33:10 -0400

    set headers even after redirect

--- http.go
+++ http.go
@@ -2,9 +2,19 @@ package main
 
 import "net/http"
 
+func redirectWrap(req *http.Request, via []*http.Request) error {
+	// spoof useragent, as there are going to be sites/servers that are
+	// setup to deny by a specific useragent string (or lack there of)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36")
+
+	return nil
+}
+
 // Get wraps the standard net/http library, allowing us to spoof hostnames and IP addresses
 func Get(url string, ip string) (*http.Response, error) {
-	client := &http.Client{}
+	client := &http.Client{
+		CheckRedirect: redirectWrap,
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 
@@ -16,13 +26,12 @@ func Get(url string, ip string) (*http.Response, error) {
 	// of note: if we plan to support custom ports, these should be rewritten
 	// within the header. E.g. "hostname.com:8080" -- though, common ports like
 	// 80 and 443 are left out.
-	if len(ip) > 0 {
-		req.Host = ip
-	}
 
-	// spoof useragent, as there are going to be sites/servers that are
-	// setup to deny by a specific useragent string (or lack there of)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36")
+	// assign the origin host to the host header value
+	req.Host = req.URL.Host
+
+	// and overwrite the host used to make the connection
+	req.URL.Host = ip
 
 	// actually make the request here
 	resp, err := client.Do(req)
