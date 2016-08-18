697b3eb5b6bee26c8eb6f455f454804ea2f00e0c
Liam Stanley <me@liamstanley.io>
2016-08-17 23:56:56 -0400

    utilize scrapers threaded functionality

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"log"
 	"runtime"
-	"sync"
 
 	df "github.com/Liamraystanley/marill/domfinder"
 	"github.com/Liamraystanley/marill/scraper"
 )
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU() * 2)
 
 	ps := df.GetProcs()
 	for _, proc := range ps {
@@ -24,30 +23,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	results := []*scraper.Results{}
-	var wg sync.WaitGroup
-
-	// loop through all supplied urls and send them to a worker to be fetched
+	tmplist := []*scraper.Domain{}
 	for _, domain := range domains {
-		wg.Add(1)
-
-		go func(domain *df.Domain) {
-			defer wg.Done()
-
-			fmt.Printf("[\033[1;36m---\033[0;m] [\033[0;32m------\033[0;m] \033[0;95mStarting to scan %s\033[0;m\n", domain.URL.String())
-			result := scraper.Crawl(domain.URL.String(), "")
-			results = append(results, result)
-
-			if result.Error != nil {
-				fmt.Printf("[\033[1;36m---\033[0;m] [\033[0;32m%4dms\033[0;m] \033[0;31mFinished scanning %s (error: %s)\033[0;m\n", result.TotalTime.Milli, domain.URL.String(), result.Error)
-			} else {
-				fmt.Printf("[\033[1;36m---\033[0;m] [\033[0;32m%4dms\033[0;m] \033[0;95mFinished scanning %s\033[0;m\n", result.TotalTime.Milli, domain.URL.String())
-			}
-		}(domain)
+		tmplist = append(tmplist, &scraper.Domain{URL: domain.URL, IP: domain.IP})
 	}
-
-	// wait for all workers to complete their tasks
-	wg.Wait()
-
-	// print out cool stuff here based on results
+	scraper.Crawl(tmplist)
 }
