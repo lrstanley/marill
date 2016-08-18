94739dbd1a65d56deee5883d6f8569ac51f31dea
Liam Stanley <me@liamstanley.io>
2016-08-18 14:56:33 -0400

    add some conventional logging

---
+++ logger.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var errorlog *os.File
+var logger *log.Logger
+
+func initLogger(w io.Writer) {
+	logger = log.New(w, "", log.Lshortfile|log.LstdFlags)
+}
+
+func initLoggerToFile(fn string) {
+	logf, err := os.OpenFile(fn, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("error opening log file: %s, %v", fn, err)
+		os.Exit(1)
+	}
+	initLogger(logf)
+}
