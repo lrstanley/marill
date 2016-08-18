a1751578e8d4b12785ac3d0e08ce3decd62880d0
Liam Stanley <me@liamstanley.io>
2016-08-18 15:19:57 -0400

    more tests

--- main_test.go
+++ main_test.go
@@ -9,25 +9,34 @@ import (
 func TestFetch(t *testing.T) {
 	cases := []struct {
 		in   string
+		inx  string // extra -- e.g. ip
 		want bool
 	}{
-		{"https://liamstanley.io", false},
-		{"https://google.com/", false},
-		{"htps://google.com", true},
-		{"http://liamstanley.io", false},
-		{"http://some-domains-that-doesnt-exist.com", true},
-		{"https://some-domains-that-doesnt-exist.com", true},
+		{"https://liamstanley.io", "", false},
+		{"https://liamstanley.io", "0.0.0.0", false},
+		{"https://liamstanley.io", "000.000.00.0", false},
+		{"https://liamstanley.io", "111.1111.11.1", false},
+		{"https://liamstanley.io", "1.1.1.1.", false},
+		{"https://google.com/", "", false},
+		{"htps://google.com", "", true},
+		{"http://liamstanley.io", "", false},
+		{"http://liamstanley.io", "0.0.0.0", false},
+		{"http://liamstanley.io", "000.000.00.0", false},
+		{"http://liamstanley.io", "111.1111.11.1", false},
+		{"http://liamstanley.io", "1.1.1.1.", false},
+		{"http://some-domains-that-doesnt-exist.com", "", true},
+		{"https://some-domains-that-doesnt-exist.com", "", true},
 	}
 
 	for _, c := range cases {
-		got := scraper.Crawl(c.in, "")
+		got := scraper.FetchURL(c.in, "")
 
 		if got.Error != nil && !c.want {
-			t.Errorf("scraper.Crawl(%q) == %q, wanted error: %v", c.in, got.Error, c.want)
+			t.Errorf("scraper.fetchURL(%q, %q) == %q, wanted error: %v", c.in, c.inx, got.Error, c.want)
 		}
 
 		if got.Error == nil && c.want && got.Code == 200 {
-			t.Errorf("scraper.Crawl(%s) == %q (%q), though no errors", c.in, got.Error, got)
+			t.Errorf("scraper.fetchURL(%q, %q) == %q (%#v), though no errors", c.in, c.inx, got.Error, got)
 		}
 	}
 
