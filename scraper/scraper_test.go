f2608abd77d2acb9170ff9b0f6914c73001e70cd
Liam Stanley <me@liamstanley.io>
2016-08-22 02:28:26 -0400

    additional http unit tests and documentation

--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -12,26 +12,33 @@ func TestFetch(t *testing.T) {
 	cases := []struct {
 		in   string
 		inx  string // extra -- e.g. ip
-		want bool
+		want bool   // true == error
 	}{
 		{"https://liamstanley.io", "", false},
-		{"https://liamstanley.io", "0.0.0.0", false},
-		{"https://liamstanley.io", "000.000.00.0", false},
-		{"https://liamstanley.io", "111.1111.11.1", false},
-		{"https://liamstanley.io", "1.1.1.1.", false},
+		{"https://liamstanley.io", "0.0.0.0", true},       // invalid ip
+		{"https://liamstanley.io", "000.000.00.0", true},  // invalid ip
+		{"https://liamstanley.io", "111.1111.11.1", true}, // invalid ip
+		{"https://liamstanley.io", "1.1.1.1.", true},      // invalid ip
 		{"https://google.com/", "", false},
-		{"htps://google.com", "", true},
+		{"htps://google.com", "", true}, // invalid schema
 		{"http://liamstanley.io", "", false},
-		{"http://liamstanley.io", "0.0.0.0", false},
-		{"http://liamstanley.io", "000.000.00.0", false},
-		{"http://liamstanley.io", "111.1111.11.1", false},
-		{"http://liamstanley.io", "1.1.1.1.", false},
-		{"http://some-domains-that-doesnt-exist.com", "", true},
-		{"https://some-domains-that-doesnt-exist.com", "", true},
+		{"http://liamstanley.io", "0.0.0.0", true},                              // invalid ip
+		{"http://liamstanley.io", "000.000.00.0", true},                         // invalid ip
+		{"http://liamstanley.io", "111.1111.11.1", true},                        // invalid ip
+		{"http://liamstanley.io", "1.1.1.1.", true},                             // invalid ip
+		{"http://some-domains-that-doesnt-exist.com/x", "", true},               // invalid domain/path
+		{"https://some-domains-that-doesnt-exist.com/x", "", true},              // invalid domain/path
+		{"https://httpbin.org/redirect/10", "", true},                           // we allow max of 3 redirects
+		{"https://httpbin.org/links/10", "", false},                             // provide some html links
+		{"https://httpbin.org/html", "", false},                                 // return some html
+		{"https://httpbin.org/drip?duration=5&numbytes=5&code=200", "", false},  // drip for 5 seconds
+		{"https://httpbin.org/drip?duration=11&numbytes=5&code=200", "", false}, // drip for 11 seconds, 10s is our timeout
+		{"https://httpbin.org/delay/12", "", true},                              // 10s is our timeout
+		{"https://httpbin.org/delay/3", "", false},
 	}
 
 	for _, c := range cases {
-		got := FetchURL(c.in, "", logger)
+		got := FetchURL(c.in, c.inx, logger)
 
 		if got.Error != nil && !c.want {
 			t.Errorf("fetchURL(%q, %q) == %q, wanted error: %v", c.in, c.inx, got.Error, c.want)
