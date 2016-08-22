5bbf9d0c81b80aa4cf37e88193d663937e22e003
Liam Stanley <me@liamstanley.io>
2016-08-22 16:49:09 -0400

    better ssl verification

--- scraper/http.go
+++ scraper/http.go
@@ -1,19 +1,26 @@
 package scraper
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"errors"
+	"net"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 // CustomClient is the state for our custom http wrapper, which houses
 // the needed data to be able to rewrite the outgoing request during
 // redirects.
 type CustomClient struct {
-	URL  string
-	IP   string
-	Host string
+	URL       string
+	IP        string
+	Host      string
+	ResultURL *url.URL
 }
 
 // CustomResponse is the wrapped response from http.Client.Do() which also
@@ -24,6 +31,8 @@ type CustomResponse struct {
 	Time *TimerResult
 	URL  string
 }
+
+var reIP = regexp.MustCompile(`^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$`)
 
 func (c *CustomClient) redirectHandler(req *http.Request, via []*http.Request) error {
 	c.requestWrap(req)
@@ -36,6 +45,10 @@ func (c *CustomClient) redirectHandler(req *http.Request, via []*http.Request) e
 	if len(via) > 3 {
 		// assume too many redirects
 		return errors.New("too many redirects (3)")
+	}
+
+	if reIP.MatchString(req.Host) && req.Host != c.IP {
+		return errors.New("Redirected to IP that doesn't match proxy")
 	}
 
 	return nil
@@ -62,14 +75,165 @@ func (c *CustomClient) requestWrap(req *http.Request) *http.Request {
 		}
 	}
 
+	// update our cached resulting uri
+	c.ResultURL = req.URL
+	if len(req.Host) > 0 {
+		c.ResultURL.Host = req.Host
+	}
+
 	return req
+}
+
+type HostnameError struct {
+	Certificate *x509.Certificate
+	Host        string
+}
+
+func (h HostnameError) Error() string {
+	c := h.Certificate
+
+	var valid string
+	if ip := net.ParseIP(h.Host); ip != nil {
+		// Trying to validate an IP
+		if len(c.IPAddresses) == 0 {
+			return "x509: cannot validate certificate for " + h.Host + " because it doesn't contain any IP SANs"
+		}
+		for _, san := range c.IPAddresses {
+			if len(valid) > 0 {
+				valid += ", "
+			}
+			valid += san.String()
+		}
+	} else {
+		if len(c.DNSNames) > 0 {
+			valid = strings.Join(c.DNSNames, ", ")
+		} else {
+			valid = c.Subject.CommonName
+		}
+	}
+	return "x509: certificate is valid for " + valid + ", not " + h.Host
+}
+
+// toLowerCaseASCII returns a lower-case version of in. See RFC 6125 6.4.1. We use
+// an explicitly ASCII function to avoid any sharp corners resulting from
+// performing Unicode operations on DNS labels.
+func toLowerCaseASCII(in string) string {
+	// If the string is already lower-case then there's nothing to do.
+	isAlreadyLowerCase := true
+	for _, c := range in {
+		if c == utf8.RuneError {
+			// If we get a UTF-8 error then there might be
+			// upper-case ASCII bytes in the invalid sequence.
+			isAlreadyLowerCase = false
+			break
+		}
+		if 'A' <= c && c <= 'Z' {
+			isAlreadyLowerCase = false
+			break
+		}
+	}
+
+	if isAlreadyLowerCase {
+		return in
+	}
+
+	out := []byte(in)
+	for i, c := range out {
+		if 'A' <= c && c <= 'Z' {
+			out[i] += 'a' - 'A'
+		}
+	}
+	return string(out)
+}
+
+func matchHostnames(pattern, host string) bool {
+	host = strings.TrimSuffix(host, ".")
+	pattern = strings.TrimSuffix(pattern, ".")
+
+	if len(pattern) == 0 || len(host) == 0 {
+		return false
+	}
+
+	patternParts := strings.Split(pattern, ".")
+	hostParts := strings.Split(host, ".")
+
+	if len(patternParts) != len(hostParts) {
+		return false
+	}
+
+	for i, patternPart := range patternParts {
+		if i == 0 && patternPart == "*" {
+			continue
+		}
+		if patternPart != hostParts[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// verifyx509 returns nil if c is a valid certificate for the named host.
+// Otherwise it returns an error describing the mismatch.
+func verifyx509(c *x509.Certificate, h string) error {
+	// IP addresses may be written in [ ].
+	candidateIP := h
+	if len(h) >= 3 && h[0] == '[' && h[len(h)-1] == ']' {
+		candidateIP = h[1 : len(h)-1]
+	}
+	if ip := net.ParseIP(candidateIP); ip != nil {
+		// We only match IP addresses against IP SANs.
+		// https://tools.ietf.org/html/rfc6125#appendix-B.2
+		for _, candidate := range c.IPAddresses {
+			if ip.Equal(candidate) {
+				return nil
+			}
+		}
+		return HostnameError{c, candidateIP}
+	}
+
+	lowered := toLowerCaseASCII(h)
+
+	if len(c.DNSNames) > 0 {
+		for _, match := range c.DNSNames {
+			if matchHostnames(toLowerCaseASCII(match), lowered) {
+				return nil
+			}
+		}
+		// If Subject Alt Name is given, we ignore the common name.
+	} else if matchHostnames(toLowerCaseASCII(c.Subject.CommonName), lowered) {
+		return nil
+	}
+
+	return HostnameError{c, h}
+}
+
+func VerifyHostname(c *tls.ConnectionState, host string) error {
+	if c == nil {
+		return nil
+	}
+
+	// if len(c.VerifiedChains) == 0 {
+	// 	return errors.New("tls: handshake did not verify certificate chain")
+	// }
+
+	return verifyx509(c.PeerCertificates[0], host)
 }
 
 // getHandler wraps the standard net/http library, allowing us to spoof hostnames and IP addresses
 func (c *CustomClient) getHandler() (*CustomResponse, error) {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			// unfortunately, ServerName will not persist over a redirect. so... we have to ignore
+			// ssl invalidations and do them somewhat manually.
+			InsecureSkipVerify: true,
+			ServerName:         c.Host,
+		},
+	}
 	client := &http.Client{
 		CheckRedirect: c.redirectHandler,
 		Timeout:       time.Duration(10) * time.Second,
+		Transport:     transport,
 	}
 
 	req, err := http.NewRequest("GET", c.URL, nil)
@@ -77,8 +241,6 @@ func (c *CustomClient) getHandler() (*CustomResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	c.Host = req.URL.Host
 
 	c.requestWrap(req)
 
@@ -94,7 +256,10 @@ func (c *CustomClient) getHandler() (*CustomResponse, error) {
 	var url string
 
 	if err == nil {
-		url = resp.Request.URL.String()
+		url = c.ResultURL.String()
+		if err := VerifyHostname(resp.TLS, c.ResultURL.Host); err != nil {
+			return nil, err
+		}
 	} else {
 		url = req.URL.String()
 	}
@@ -106,7 +271,12 @@ func (c *CustomClient) getHandler() (*CustomResponse, error) {
 
 // Get wraps GetHandler -- easy interface for making get requests
 func Get(url string, ip string) (*CustomResponse, error) {
-	c := &CustomClient{URL: url, IP: ip}
+	host, err := getHost(url)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &CustomClient{URL: url, IP: ip, Host: host}
 
 	return c.getHandler()
 }
