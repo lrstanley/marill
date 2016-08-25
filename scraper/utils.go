1d83b6130953cdffc035c66d1e5e0bdab5131022
Liam Stanley <me@liamstanley.io>
2016-08-25 16:29:14 -0400

    add stripDups as a way to filter duplicate domains

--- scraper/utils.go
+++ scraper/utils.go
@@ -54,3 +54,25 @@ func getHost(uri string) (string, error) {
 
 	return host.Host, nil
 }
+
+// stripDups strips all domains that have the same resulting URL/IP
+func stripDups(domains *[]*Domain) {
+	var tmp []*Domain
+
+	for _, dom := range *domains {
+		isIn := false
+		for _, other := range tmp {
+			if dom.URL.String() == other.URL.String() && dom.IP == other.IP {
+				isIn = true
+				break
+			}
+		}
+		if !isIn {
+			tmp = append(tmp, dom)
+		}
+	}
+
+	*domains = tmp
+
+	return
+}
