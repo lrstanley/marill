7ddf73727d2ec6dc5a661757eadf22dc484e48e0
Liam Stanley <me@liamstanley.io>
2016-08-28 20:07:39 -0400

    add color logging functionality

--- logger.go
+++ logger.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // may just setup a global logger, and change output during runtime...
@@ -33,4 +35,46 @@ func initLoggerToFile(fn string) {
 
 func closeLogger() {
 	logf.Close()
+}
+
+type Color struct {
+	Name string
+	ID   int
+}
+
+var colors = []*Color{
+	{"c", 0}, {"bold", 1}, {"black", 30}, {"red", 31}, {"green", 32}, {"yellow", 33},
+	{"blue", 34}, {"magenta", 35}, {"cyan", 36}, {"white", 37}, {"gray", 90},
+	{"lightred", 91}, {"lightgreen", 92}, {"lightyellow", 93}, {"lightblue", 94},
+	{"lightmagenta", 95}, {"lightcyan", 96}, {"lightgray", 97},
+}
+
+func FmtColor(format *string, shouldStrip bool) {
+	for _, clr := range colors {
+		if shouldStrip {
+			*format = strings.Replace(*format, "{"+clr.Name+"}", "", -1)
+		} else {
+			*format = strings.Replace(*format, "{"+clr.Name+"}", "\x1b["+strconv.Itoa(clr.ID)+"m", -1)
+		}
+	}
+
+	if !shouldStrip {
+		*format = *format + "\x1b[0;m"
+	}
+
+	return
+}
+
+type Output struct{}
+
+// Printf interprets []*Color{} escape codes and prints them to stdout
+func (o Output) Printf(format string, a ...interface{}) (n int, err error) {
+	FmtColor(&format, conf.out.noColors)
+
+	return fmt.Printf(format, a...)
+}
+
+// Println interprets []*Color{} escape codes and prints them to stdout
+func (o Output) Println(a ...interface{}) (n int, err error) {
+	return fmt.Println(a...)
 }
