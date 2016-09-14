667562ef78b90f01f465f99d195531070921cd20
Liam Stanley <me@liamstanley.io>
2016-09-14 01:52:07 -0400

    add unit tests for duplication stripping

--- domfinder/utils_test.go
+++ domfinder/utils_test.go
@@ -21,6 +21,67 @@ func TestGetHostname(t *testing.T) {
 	return
 }
 
+func TestStripDups(t *testing.T) {
+	pprint := func(domains []*Domain) string {
+		out := make([]string, len(domains))
+
+		for i, dom := range domains {
+			out[i] = dom.String()
+		}
+
+		return strings.Join(out, ", ")
+	}
+
+	run := func(domains []*Domain, length int) {
+		orig := domains
+		if stripDups(&domains); len(domains) != length {
+			t.Fatalf("stripDups(%s) == %s, wanted %d", pprint(orig), pprint(domains), length)
+		}
+	}
+
+	// regular duplicate
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+	}, 1)
+
+	// multiple duplicates
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+	}, 1)
+
+	// "some" duplicates
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain1.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain1.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+	}, 2)
+
+	// different ports
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "8080", URL: MustURL("domain.com", "8080")},
+	}, 2)
+
+	// different IPs
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.5", Port: "80", URL: MustURL("domain.com", "80")},
+	}, 1)
+
+	// different domains
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain1.com", "80")},
+	}, 2)
+
+	return
+}
+
 func TestIsDomainURL(t *testing.T) {
 	cases := []struct {
 		host string // host uri
