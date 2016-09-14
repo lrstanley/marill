b83bf6ca95640b1c7339fae70960fb5fab4e06a3
Liam Stanley <me@liamstanley.io>
2016-09-14 15:01:24 -0400

    add tests for filtering

---
+++ domfinder/domfinder_test.go
@@ -0,0 +1,69 @@
+package domfinder
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFilter(t *testing.T) {
+	pprint := func(domains []*Domain) string {
+		out := make([]string, len(domains))
+
+		for i, dom := range domains {
+			out[i] = dom.String()
+		}
+
+		return strings.Join(out, ", ")
+	}
+
+	run := func(domains []*Domain, cnf DomainFilter, length int) {
+		f := &Finder{Domains: domains}
+		if f.Filter(cnf); len(f.Domains) != length {
+			t.Fatalf("Filter(%s) == %s, wanted %d", pprint(domains), pprint(f.Domains), length)
+		}
+	}
+
+	// should match all
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+	}, DomainFilter{IgnoreHTTP: true}, 0)
+
+	// should match none
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+	}, DomainFilter{IgnoreHTTPS: true}, 2)
+
+	// should match one
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "443", URL: MustURL("domain.com", "443")},
+	}, DomainFilter{IgnoreHTTPS: true}, 1)
+
+	// should match * (all)
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "443", URL: MustURL("domain.com", "443")},
+	}, DomainFilter{MatchOnly: "*"}, 2)
+
+	// should match none
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "443", URL: MustURL("domain.com", "443")},
+	}, DomainFilter{IgnoreMatch: "*"}, 0)
+
+	// should match none
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "443", URL: MustURL("domain.com", "443")},
+	}, DomainFilter{IgnoreMatch: "*domain.com*"}, 0)
+
+	// should match one
+	run([]*Domain{
+		{IP: "1.2.3.4", Port: "80", URL: MustURL("domain.com", "80")},
+		{IP: "1.2.3.4", Port: "443", URL: MustURL("domain.com", "443")},
+	}, DomainFilter{IgnoreMatch: "https://*"}, 1)
+
+	return
+}
