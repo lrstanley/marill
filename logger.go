3f7af151eabf02edd291990b2665eeb1ff2429c2
Liam Stanley <me@liamstanley.io>
2016-10-04 03:05:41 -0400

    add support to redirect out.* to any io.Writer

--- logger.go
+++ logger.go
@@ -14,8 +14,7 @@ import (
 	"strings"
 )
 
-// may just setup a global logger, and change output during runtime...
-// http://codereview.stackexchange.com/a/59733
+// just setup a global logger, and change output during runtime...
 
 var logf *os.File
 var logger *log.Logger
@@ -99,38 +98,63 @@ func FmtColor(format *string, shouldStrip bool) {
 }
 
 // Output is the bare out struct for which stdout messages are passed to
-type Output struct{}
+type Output struct {
+	log    *log.Logger
+	buffer []string
+}
+
+var out = Output{}
+
+func initOut(w io.Writer) {
+	out.log = log.New(w, "", 0)
+}
+
+func (o *Output) Write(b []byte) (int, error) {
+	str := fmt.Sprintf("%s", b)
+	o.log.Print(str)
+	o.AddLog(str)
+
+	return len(b), nil
+}
+
+func (o *Output) AddLog(line string) {
+	o.buffer = append(o.buffer, line)
+}
 
 // Printf interprets []*Color{} escape codes and prints them to stdout
-func (o Output) Printf(format string, a ...interface{}) (n int, err error) {
+func (o *Output) Printf(format string, a ...interface{}) {
 	if conf.out.ignoreStd {
-		return 0, nil
+		return
 	}
 
 	str := fmt.Sprintf(format, a...)
 	FmtColor(&str, conf.out.noColors)
 
-	return fmt.Print(str)
+	out.log.Print(str)
+	o.AddLog(str)
 }
 
 // Println interprets []*Color{} escape codes and prints them to stdout
-func (o Output) Println(a ...interface{}) (n int, err error) {
+func (o *Output) Println(a ...interface{}) {
 	if conf.out.ignoreStd {
-		return 0, nil
+		return
 	}
 
 	str := fmt.Sprintln(a...)
 	FmtColor(&str, conf.out.noColors)
-	return fmt.Print(str)
+
+	out.log.Print(str)
+	o.AddLog(str)
 }
 
 // Fatalf interprets []*Color{} escape codes and prints them to stdout/logger, and exits
-func (o Output) Fatalf(format string, a ...interface{}) {
+func (o *Output) Fatalf(format string, a ...interface{}) {
 	// print to regular stdout
 	if !conf.out.ignoreStd {
 		str := fmt.Sprintf(fmt.Sprintf("{bold}{red}error:{c} %s\n", format), a...)
 		FmtColor(&str, conf.out.noColors)
-		fmt.Print(str)
+		out.log.Print(str)
+		o.AddLog(str)
 	}
 
 	// strip color from format
@@ -139,12 +163,13 @@ func (o Output) Fatalf(format string, a ...interface{}) {
 }
 
 // Fatal interprets []*Color{} escape codes and prints them to stdout, and exits
-func (o Output) Fatal(a ...interface{}) {
+func (o *Output) Fatal(a ...interface{}) {
 	// print to regular stdout
 	if !conf.out.ignoreStd {
 		str := fmt.Sprintf("{bold}{red}error:{c} %s", fmt.Sprintln(a...))
 		FmtColor(&str, conf.out.noColors)
-		fmt.Print(str)
+		out.log.Print(str)
+		o.AddLog(str)
 	}
 
 	str := fmt.Sprintln(a...)
